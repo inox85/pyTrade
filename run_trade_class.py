2c194b98fca8fad494f92de2feca19d9ef11cad8
Davide Innocenti <d.innocenti@chemitec.it>
2025-09-09 12:27:55 +0200

    Modifiche varie

--- run_trade_class.py
+++ run_trade_class.py
@@ -1,4 +1,4 @@
-from data_preparation import DataPreprocessor
+from data_preprocessor import DataPreprocessor
 from data_downloader import DataDownloader
 from dukascopy_python.instruments import INSTRUMENT_US_AAPL_US_USD, INSTRUMENT_US_PLTR_US_USD
 import dukascopy_python 
@@ -25,29 +25,27 @@ def main():
         end_date = end_d.strftime("%Y-%m-%dT%H:%M:%SZ")
 
         print("Dowload data per:", symbol)
-        
+
         df_history = alpaca.download_data("1H", start_date=start_date, end_date=end_date )
 
-        prepocessor = DataPreprocessor(df_history, symbol=symbol, interval="1H")
+        preprocessor = DataPreprocessor(df_history, symbol=symbol, interval="1H")
 
-        df = prepocessor.get_dataset()
+        df = preprocessor.get_dataset()
 
-        df_tecnical = prepocessor.generate_tecnical_dataset(df)
+        print("Database di origine:", df)
 
-        
+        df_tecnical = preprocessor.generate_tecnical_dataset(df)
 
-        # for instrument in instruments:
-        #     print("Downloading data for:", instrument)
-        #     df_history = DataDownloader.download_data_to_dataframe(
-        #         instrument,
-        #         interval=dukascopy_python.INTERVAL_HOUR_1,
-        #         start=datetime(2024, 8, 6),
-        #         end=datetime.now()
-        #     )
-        #     prepocessor = DataPreprocessor(df_history, symbol=instrument, interval=dukascopy_python.INTERVAL_HOUR_1)
-        #     prepocessor.generate_dataset()
-        #     prepocessor.generate_targets()
-        #     prepocessor.show_full_dataframe()
+        print("Database di tecnico:", df_tecnical)
+
+        df_targets = preprocessor.generate_targets_dataset(df_tecnical)
+        print("Database di targets:", df)
+
+        df.to_csv(f"data/{symbol}_processed.csv", sep=';', encoding='utf-8')
+
+        print(df_targets)
+
+
 
 if __name__ == "__main__":      
     main()
